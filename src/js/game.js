723b4024d7a86afb071526b6301a1e572a6fdead
Olesya Zubkova <olesya_2802@mail.ru>
2021-10-13 11:50:06 +0300

    task changed

---
+++ src/js/game.js
@@ -0,0 +1,20 @@
+import character from './domain.js';
+
+class Game {
+    start() {
+      console.log('game started');
+    }
+  }
+  
+  const defaultGame = new Game();
+  export default defaultGame;
+  
+  export class GameSavingData {
+  }
+  
+  export function readGameSaving() {
+  }
+  
+  export function writeGameSaving() {
+  }
+
